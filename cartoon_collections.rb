a1699e2f3bb679098e177804c3b2d1dfabf583c6
James Bridges <masterbridges@gmail.com>
2019-12-23 19:14:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  index = foods.include? {|food| food == cheese_types}
+  index = foods.include? {|food| cheese_types}
   binding.pry
 
 end
