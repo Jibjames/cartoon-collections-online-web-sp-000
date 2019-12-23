6b39ce203c9a35ebfe784f0702a7e706bf0e340d
James Bridges <masterbridges@gmail.com>
2019-12-23 18:40:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,11 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements)# code an argument here
   # Your code here
+  elements.map do |element|
+    "#{element.capitalize}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
